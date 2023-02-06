a0463c55771ba698a13518dd59892d12fcaeb523
Gabo748 <gaeducampos@gmail.com>
2023-02-06 11:05:07 -0600

    Initial Commit

---
+++ Rick_And_Morty_Programmatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick_And_Morty_Programmatic
+//
+//  Created by Gabriel Campos on 6/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
